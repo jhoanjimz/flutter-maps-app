6b61a237f4a049df2f0a95df17c1d87b4b93cada
jhoanjimz <jhoan.developer@gmail.com>
2023-08-27 15:58:49 -0500

    Leer coordenadas, Setear Mapa, Diseño de mapa, Snackbar

--- lib/screens/map_screen.dart
+++ lib/screens/map_screen.dart
@@ -1,13 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:maps_app/blocs/blocs.dart';
+import 'package:maps_app/views/views.dart';
 
-class MapScreen extends StatelessWidget {
+import '../widgets/widgets.dart';
+
+class MapScreen extends StatefulWidget {
   const MapScreen({Key? key}) : super(key: key);
-  
+
+  @override
+  State<MapScreen> createState() => _MapScreenState();
+}
+
+class _MapScreenState extends State<MapScreen> {
+
+  late LocationBloc locationBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    locationBloc = BlocProvider.of<LocationBloc>(context);
+    // locationBloc.getCurrentPosition();
+    locationBloc.startFollowingUser();
+  }
+
+  @override
+  void dispose() {
+    locationBloc.stopFollowingUser();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-         child: Text('MapScreen'),
+    return Scaffold(
+      body: BlocBuilder<LocationBloc, LocationState>(
+        builder: (context, state) {
+
+          if( state.lastKnowLocation == null ) return const Center( child: Text('Espere por favor...'));
+          
+          return SingleChildScrollView(
+            child: Stack(
+              children: [
+                MapView( initialLocation: state.lastKnowLocation! )
+              ],
+            ),
+          );
+        }
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: const Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          BtnCurrentLocation()
+        ],
       ),
     );
   }
